3949a0a68eaad7de6d518b93994426ac74db413f
MahindraGamini <saimahindra06@gmail.com>
2024-10-10 13:21:10 +0530

    defa dark

--- app/context/ThemeContext.tsx
+++ app/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { createContext, useState, useEffect, ReactNode } from 'react';
 
 interface ThemeContextProps {
@@ -7,7 +7,7 @@ interface ThemeContextProps {
 }
 
 export const ThemeContext = createContext<ThemeContextProps>({
-  theme: 'light',
+  theme: 'dark',
   toggleTheme: () => {},
 });
 
@@ -16,19 +16,22 @@ interface ThemeProviderProps {
 }
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [theme, setTheme] = useState<string>('light');
+  const [theme, setTheme] = useState<string>('dark'); // Default theme is 'dark'
 
   useEffect(() => {
-
     const storedTheme = localStorage.getItem('theme');
+
     if (storedTheme) {
       setTheme(storedTheme);
-      document.documentElement.classList.toggle('dark', storedTheme === 'dark');
+      document.documentElement.classList.toggle('light', storedTheme === 'light');
+    } else {
+      setTheme('dark'); // Explicitly set default theme to dark
+      document.documentElement.classList.add('dark'); // Ensure dark theme is applied initially
     }
   }, []);
 
   const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
     setTheme(newTheme);
     document.documentElement.classList.toggle('dark', newTheme === 'dark');
     localStorage.setItem('theme', newTheme);
